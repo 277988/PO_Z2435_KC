d0c4681f3fdc63c22586debed095fda28216dced
277988 <277988@student.pwr.edu.pl>
2024-10-29 17:46:02 +0100

    Point1

---
+++ Point/src/Square.java
@@ -0,0 +1,17 @@
+public class Square extends Shape {
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double area() {
+        return side * side;
+    }
+
+    @Override
+    public double circumference() {
+        return 4 * side;
+    }
+}
